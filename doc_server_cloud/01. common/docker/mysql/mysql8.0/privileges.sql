2da38ccb56282d37e1f0bf8aa2a80c0f1ebf7022
everforcc <718497737@qq.com>
2024-04-22 17:53:33 +0800

    20240422 nacos多数据源配置

--- doc_server_cloud/01. common/docker/mysql/mysql8.0/privileges.sql
+++ doc_server_cloud/01. common/docker/mysql/mysql8.0/privileges.sql
@@ -7,7 +7,13 @@ ALTER USER 'cceverfor'@'%' IDENTIFIED WITH mysql_native_password BY '5664c.c.';
 -- CREATE USER 'slave'@'119.3.239.81' IDENTIFIED WITH sha256_password BY '5664c.c.';
 -- 基础项目表权限
 GRANT ALL PRIVILEGES ON `oneforall`.* TO 'cceverfor'@'%' ;
+-- hkzk
+GRANT ALL PRIVILEGES ON `hkzkcrcc`.* TO 'cceverfor'@'%' ;
 -- Apollo
 GRANT ALL PRIVILEGES ON `ApolloConfigDB`.* TO 'cceverfor'@'%' ;
 GRANT ALL PRIVILEGES ON `ApolloPortalDB`.* TO 'cceverfor'@'%' ;
+-- 若依
+GRANT ALL PRIVILEGES ON `rycloud`.* TO 'cceverfor'@'%' ;
+-- nacos
+GRANT ALL PRIVILEGES ON `nacosconfig`.* TO 'cceverfor'@'%' ;
 flush privileges;
