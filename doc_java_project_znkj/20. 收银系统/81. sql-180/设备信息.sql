7a0fb1aa3589d8a0bbf9c1ac143df92f5191dce3
c.c <718497737@qq.com>
2024-10-14 17:14:25 +0800

    znkj脚本备份

---
+++ doc_java_project_znkj/20. 收银系统/81. sql-180/设备信息.sql
@@ -0,0 +1,4 @@
+
+-- 原来的值 shouyinceshi
+SELECT e.device_sn, e.zn_device_sn_var, e.*
+FROM `zn_local` e;
---
+++ doc_java_project_znkj/20. 收银系统/81. sql-180/购物车.sql
@@ -0,0 +1,13 @@
+-- 查询用户信息
+SELECT * FROM mt_user;
+SELECT * FROM t_account;
+
+-- 购物车
+SELECT * FROM mt_cart;
+SELECT * FROM mt_cart e WHERE e.hang_no = '#07' ;
+
+-- 查询绑定的商品是否存在
+SELECT e.is_single_spec,e.* FROM mt_goods e 
+WHERE e.type = 'service' AND e.status = 'A' AND e.id = 576;
+
+SELECT * FROM mt_goods t WHERE t.STATUS = 'A' AND t.type = 'service' AND t.id = ?;
