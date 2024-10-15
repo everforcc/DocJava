c3bf07f4e56695c78d9727204a3a2475da398bca
c.c <718497737@qq.com>
2024-10-15 19:02:26 +0800

    mq 工作模式

---
+++ doc_java_project_znkj/20. 收银系统/80. sql-local/商品.sql
@@ -0,0 +1,4 @@
+-- 商品
+SELECT e.LOGO,e.IMAGES,e.CREATE_TIME,e.* FROM mt_goods e
+ORDER BY e.CREATE_TIME DESC;
+
---
+++ doc_java_project_znkj/20. 收银系统/81. sql-180/商品.sql
@@ -0,0 +1,5 @@
+-- 商品
+SELECT e.LOGO,e.IMAGES,e.CREATE_TIME,e.* FROM mt_goods e
+ORDER BY e.CREATE_TIME DESC;
+
+select * from mt_goods_cate e;
