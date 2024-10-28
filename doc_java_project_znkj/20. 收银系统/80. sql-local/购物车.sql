181c5d441416b0179b72d07fb6468395785132f1
c.c <718497737@qq.com>
2024-10-28 20:41:45 +0800

    znkj 每日

--- doc_java_project_znkj/20. 收银系统/80. sql-local/购物车.sql
+++ doc_java_project_znkj/20. 收银系统/80. sql-local/购物车.sql
@@ -10,7 +10,7 @@ FROM mt_cart e
 order by e.CREATE_TIME desc;
 
 -- 挂单
-SELECT *
+SELECT e.GOODS_ID
 FROM mt_cart e
 WHERE e.hang_no = '#07';
 
@@ -20,3 +20,14 @@ FROM mt_goods e
 WHERE e.type = 'service'
   AND e.status = 'A'
   AND e.id = 576;
+
+select e.*
+FROM mt_goods e
+where e.ID  in (569,2,571);
+
+select e.*
+FROM mt_goods e
+where e.id = 602;
+
+select e.id,e.CREATE_TIME,e.NUM,e.* from mt_cart e
+order by e.CREATE_TIME desc;
