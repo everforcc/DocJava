25e0523e1b6d0f4f1e552404478478ffed777a9c
c.c <718497737@qq.com>
2024-11-09 17:14:50 +0800

    移除todo，整理清单目录

--- doc_java_project_znkj/20. 收银系统/81. sql-180/卡券.sql
+++ doc_java_project_znkj/20. 收银系统/81. sql-180/卡券.sql
@@ -1,9 +1,24 @@
 
 -- 卡券表
-SELECT * FROM mt_coupon e;
+SELECT e.STATUS,e.* FROM mt_coupon e
+where e.STATUS = 'D';
+
 -- 卡券商品表
 SELECT * FROM mt_coupon_goods;
+SELECT * FROM mt_coupon_goods e
+where e.GOODS_ID = 714;
+
+
 -- 优惠券组
 SELECT * FROM mt_coupon_group;
 -- 会员卡券表
-SELECT * FROM mt_user_coupon e;
+SELECT * FROM mt_user_coupon e
+where e.COUPON_ID = 60
+and e.STATUS = 'B';
+
+-- 714
+
+SELECT e.STATUS,e.* FROM mt_coupon e
+where e.ID = 87;
+SELECT e.STATUS,e.* FROM mt_coupon_goods e
+where e.COUPON_ID = 87;
