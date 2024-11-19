f6c36dba8456b382297faeed2b00a6ca802c478b
c.c <718497737@qq.com>
2024-11-19 20:58:41 +0800

    mybatis plus 注解

--- doc_java_project_znkj/20. 收银系统/80. sql-local/temp.sql
+++ doc_java_project_znkj/20. 收银系统/80. sql-local/temp.sql
@@ -1,43 +1,9 @@
-select * from mt_balance e
-order by e.ID desc;
-select * from mt_goods ;
--- WEIGHT             decimal(10, 2)          default 0.00      null comment '重量',
-select * from mt_stock_item e
-where e.;
+UPDATE MT_USER T
+SET up_flag=0,up_count=0,
+    T.MOBILE = '15648527913',
+    up_flag=1
+WHERE T.ID = 1858711585212219394;
 
-select e.role_ids,e.* from t_account e;
-select * from t_account_duty e
-where e.acct_id = 14;
-
-select * from t_duty_source e
-where e.source_id = 125;
-
-select * from t_source e
-where e.source_name like '%收银%' ;
-
-update t_source e set e.status = 'A'
-where e.parent_id = 125;
-update t_source e set e.source_name = '收银台'
-where e.source_id = 125;
-
-select * from t_duty e;
-
-select e.* from t_account e;
-
-
-select distinct t.source_id from t_duty_source t left join t_account_duty d
-                                                           on t.duty_id = d.duty_id
-where d.acct_id = 14;
-
-
--- 订单
-select e.PAY_AMOUNT,e.AMOUNT,e.CREATE_TIME,e.PAY_TYPE,e.USER_ID,e.* from mt_order e
-order by e.CREATE_TIME desc;
-
--- 余额相关信息
-SELECT * FROM mt_balance e
-order by e.CREATE_TIME desc;
-
--- 会员余额
-select e.BALANCE,e.* from mt_user e
-where e.ID = 69;
+select e.MOBILE,E.up_flag,e.* from mt_user e
+where e.ID = 1858711585212219394
+order by e.id desc;
