317c0be8f364c7c151d8db5677e52dc01a01ed1c
c.c <718497737@qq.com>
2024-11-22 15:56:44 +0800

    znkj mt_user_coupon同步，订单脚本

---
+++ doc_java_project_znkj/02. 数据同步/sql/收银/add/add_mt_user_coupon.sql
@@ -0,0 +1,21 @@
+-- 1. 会员
+-- show create table mt_user_coupon;
+select * from mt_user_coupon;
+
+-- 2. 修改主键数据类型
+--                                  `ID` int    NOT NULL COMMENT '自增ID',
+ALTER TABLE `mt_user_coupon` MODIFY `ID` bigint NOT NULL COMMENT 'ID';
+
+-- 3. 增加字段
+ALTER TABLE `mt_user_coupon` ADD `up_flag` int DEFAULT '0' COMMENT '0 未上传 1 上传中 2 已上传';
+ALTER TABLE `mt_user_coupon` ADD `up_count` int DEFAULT '0' COMMENT '上传次数';
+ALTER TABLE `mt_user_coupon` ADD `up_msg` varchar(255) DEFAULT NULL COMMENT '上传信息';
+ALTER TABLE `mt_user_coupon` ADD `up_id` varchar(255) DEFAULT NULL COMMENT '上传ID';
+ALTER TABLE `mt_user_coupon` ADD `zn_device_sn_var` varchar(255) DEFAULT NULL COMMENT '设备编号';
+ALTER TABLE `mt_user_coupon` ADD `remark` varchar(100) DEFAULT NULL COMMENT '备注';
+ALTER TABLE `mt_user_coupon` ADD `del_flag` tinyint DEFAULT '0' COMMENT '删除标识';
+ALTER TABLE `mt_user_coupon` ADD `create_id` BIGINT(20) DEFAULT NULL COMMENT '创建者ID';
+ALTER TABLE `mt_user_coupon` ADD  `create_by` VARCHAR(50) DEFAULT NULL COMMENT '创建者';
+ALTER TABLE `mt_user_coupon` ADD  `update_id` BIGINT(20) DEFAULT NULL COMMENT '更新者ID';
+ALTER TABLE `mt_user_coupon` ADD  `update_by` VARCHAR(50) DEFAULT NULL COMMENT '更新者';
+ALTER TABLE `mt_user_coupon` ADD  `create_dept` BIGINT(20) DEFAULT NULL COMMENT '创建者部门';
