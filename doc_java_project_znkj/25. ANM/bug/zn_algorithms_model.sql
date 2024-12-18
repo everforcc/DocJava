6139a10cee57ab63f886bf26e4b0e7ac08908a44
c.c <718497737@qq.com>
2024-12-18 21:14:10 +0800

    每日

--- doc_java_project_znkj/25. ANM/bug/zn_algorithms_model.sql
+++ doc_java_project_znkj/25. ANM/bug/zn_algorithms_model.sql
@@ -1,4 +1,9 @@
-
 -- 可接入算法次数
 -- 128
-SELECT e.`algorithms_allow`,e.* FROM zn_algorithms_model e ;
+SELECT e.`algorithms_allow`,e.algorithm_type,e.core_type, e.*
+FROM zn_algorithms_model e
+where e.algorithm_type = '1' and e.core_type = 'bm1684X';
+
+select mc.max_support_algorithm,mc.algorithm_type, mc.core_type, mc.*
+from zn_core_model_capacity mc
+where mc.algorithm_type = '1' and mc.core_type = 'bm1684X';
