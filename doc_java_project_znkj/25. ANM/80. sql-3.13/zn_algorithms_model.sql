50220cc9b0aefce104fb8b46643b33b42656d049
c.c <718497737@qq.com>
2024-12-24 21:41:32 +0800

    每日

--- doc_java_project_znkj/25. ANM/80. sql-3.13/zn_algorithms_model.sql
+++ doc_java_project_znkj/25. ANM/80. sql-3.13/zn_algorithms_model.sql
@@ -8,6 +8,10 @@ select e.core_id,e.* from zn_algorithms_model e ;
 select * from zn_algorithms_config e
 where e.core_id = 27;
 
---
-SELECT * FROM zn_algorithms_model
-WHERE del_flag=0 AND algorithm_type = 1 AND id <> 1866378124951154688;
+-- 可接入算法次数
+-- 数据来源不一样
+SELECT e.algorithms_allow,e.algorithm_type,e.* FROM zn_algorithms_model e
+WHERE e.del_flag=0
+  AND e.algorithm_type = 1 AND e.id <> 1866378124951154688;
+-- 可接入算法次数
+-- 算法部署次数: /左边列表的值
