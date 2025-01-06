f909a1feeea2481f5f4521586f7ee045dda50508
c.c <718497737@qq.com>
2025-01-06 23:15:54 +0800

    每日

--- doc_java_project_znkj/21. 园区/80. sql-136-园区/租赁.sql
+++ doc_java_project_znkj/21. 园区/80. sql-136-园区/租赁.sql
@@ -16,7 +16,7 @@ left join sys_user su on zdl.dealer_id = su.user_id;
 -- 2. 经销商名下的所有sn
 -- 可能未激活
 -- group_id
-select e.dealer_id,e.* from zn_device_lease e;
+select e.group_id,e.dealer_id,e.* from zn_device_lease e;
 -- 已激活
 -- select e.* from zn_device_sn e;
 -- select * from sys_user e ;
@@ -24,15 +24,13 @@ select e.dealer_id,e.* from zn_device_lease e;
 -- 3. 事件处理人员表
 -- 使用sn相关联
 -- ,zeps.group_id
-select zeps.zn_device_sn_var,zeps.dealer_Id,zeps.* from zn_event_process_staff zeps ;
+select zeps.group_id,zeps.zn_device_sn_var,zeps.dealer_Id,zeps.* from zn_event_process_staff zeps ;
 -- 移除设备sn
-select zepsg.* from zn_event_process_staff_group zepsg ;
+-- select zepsg.* from zn_event_process_staff_group zepsg ;
 -- select * from zn_device_lease;
 
-select su.user_id,su.* from sys_user su ;
-
 -- 4. 事件表
-select zaedd.algorithm_id,zaedd.* from zn_ais_event_data_details zaedd;
+select zaedd.algorithm_id,zaedd.* from zn_ais_event_data_details_operate zaedd;
 -- zn_ais_event_data_details_operate
 --
 -- select zed.algorithm_id,zed.* from zn_event_data zed;
