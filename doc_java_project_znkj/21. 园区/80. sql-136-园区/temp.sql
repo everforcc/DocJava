28f35b9fd865820004c00aa309ab6c811666665a
c.c <718497737@qq.com>
2025-01-08 21:50:56 +0800

    每日

--- doc_java_project_znkj/21. 园区/80. sql-136-园区/temp.sql
+++ doc_java_project_znkj/21. 园区/80. sql-136-园区/temp.sql
@@ -16,3 +16,20 @@ WHERE e.req_date = '20241107';
 
 -- 设备信息
 select * from zn_local e ;
+
+
+SELECT
+    e.*, a.contact_person leaseName, a.phone leasePhone
+FROM
+    zn_ais_event_data_details_operate e
+        left join zn_device_lease a
+            on e.zn_device_sn_var = a.zn_device_sn_var
+WHERE
+        e.id = 2 and e.process_id = ?;
+
+SELECT e.acquire_time, e.* FROM zn_ais_event_data_details_operate e
+WHERE del_flag=0 AND zn_device_sn_var = 'csff001' AND status = 1 AND acquire_time < ?;
+-- csff001(String), 1(Integer), 2025-01-07T16:23:36.489930(LocalDateTime)
+
+SELECT COUNT(*) AS total FROM zn_ais_event_data_details_operate
+WHERE del_flag = 0 AND status = 0 AND zn_device_sn_var IN ('csqd001') AND create_time > ?;
