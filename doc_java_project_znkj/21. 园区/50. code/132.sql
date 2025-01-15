c4f68c871fa2a48299a77bd67e78cbf5c51cd3bd
c.c <718497737@qq.com>
2025-01-15 18:17:49 +0800

    project 文档调整

---
+++ doc_java_project_znkj/21. 园区/50. code/132.sql
@@ -0,0 +1,4 @@
+
+-- 132 ÍË¿îµÄ±í
+select e.hf_seq_id,e.trans_amt,e.create_by,e.req_date,e.* from znkj_huifu_info e
+order by e.create_time desc;
---
+++ doc_java_project_znkj/21. 园区/50. code/136.sql
@@ -0,0 +1,16 @@
+
+
+SELECT e.other_data,e.* FROM zn_pay_order_record e
+WHERE e.req_date = '20250114'
+ORDER BY e.create_time DESC;
+
+
+SELECT e.other_data,e.create_time,e.* FROM zn_pay_order_record e
+where e.del_flag = 0
+ORDER BY e.create_time DESC;
+
+
+select e.create_time,e.* from zn_pay_order e
+order by e.create_time desc;
+
+
