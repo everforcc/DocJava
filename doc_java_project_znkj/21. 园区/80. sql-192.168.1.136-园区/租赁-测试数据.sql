98670d0743bf7c11fdafc2928f9f81406e592095
c.c <718497737@qq.com>
2025-01-18 21:31:30 +0800

    znkj 每日

--- doc_java_project_znkj/21. 园区/80. sql-192.168.1.136-园区/租赁-测试数据.sql
+++ doc_java_project_znkj/21. 园区/80. sql-192.168.1.136-园区/租赁-测试数据.sql
@@ -1,13 +1,15 @@
 
 select * from zn_event_process_rule e ;
 
-select e.group_id,e.* from zn_device_lease e;
+select e.group_id,e.manual_watch,e.del_flag,e.* from zn_device_lease e
+where e.zn_device_sn_var = 'csff001'
+;
 
 -- 重置测试数据为可查询
 select zaedd.status,zaedd.create_time,zaedd.process_id,zaedd.* from zn_ais_event_data_details_operate zaedd
 where zaedd.zn_device_sn_var = 'csff001';
 
---
+-- 重置某个设备下的所有事件
 update zn_ais_event_data_details_operate
 set process_id=null,
     status = 0,
@@ -27,6 +29,18 @@ set process_id= null,
     status = 0,
     create_time=now()
 where id in (11,12,13,14,15,16,17,18,19,20,21);
+
+-- 重置事件
+update zn_ais_event_data_details_operate
+set zn_device_sn_var = 'csff001',
+    process_id=null,
+    acquire_time=null,
+    process_time=null,
+    process_text=null,
+    process_picture=null,
+    status = 0,
+    create_time=now()
+where id in  (87,88,89,90,91,92,93,94,95,96);
 
 -- 分发数据
 update zn_ais_event_data_details_operate
