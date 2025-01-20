42a3ef86b05ea8923f9e7ad70953ff1c80e0199b
c.c <718497737@qq.com>
2025-01-20 21:00:10 +0800

    JSON_UNQUOTE

--- doc_java_project_znkj/21. 园区/13. 支付/退款/136.sql
+++ doc_java_project_znkj/21. 园区/13. 支付/退款/136.sql
@@ -1,16 +1,14 @@
 
 -- Ô°ÇøÔÆ¶Ë 192.168.1.136 ÍË¿î
 SELECT JSON_EXTRACT(e.other_data,'$.hf_seq_id'),e.pay_amt,e.req_date,e.create_time,e.other_data,e.* FROM zn_pay_order_record e
-WHERE e.req_date = '20250118'
+WHERE e.req_date = '20250120'
 ORDER BY e.create_time DESC;
 
-
+--
 SELECT e.other_data,e.create_time,e.* FROM zn_pay_order_record e
 where e.del_flag = 0
 ORDER BY e.create_time DESC;
 
-
+--
 select e.create_time,e.* from zn_pay_order e
 order by e.create_time desc;
-
-
