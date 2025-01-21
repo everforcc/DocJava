693eeb09a015239e9380df93c9226b7f8d9e21a2
c.c <718497737@qq.com>
2025-01-21 21:13:13 +0800

    nio

--- doc_java_project_znkj/21. 园区/13. 支付/退款/132.sql
+++ doc_java_project_znkj/21. 园区/13. 支付/退款/132.sql
@@ -1,5 +1,5 @@
 
 --  ¶©µ¥ 192.168.1.132 ÍË¿î
 select e.hf_seq_id,e.trans_amt,e.create_by,e.req_date,e.* from znkj_huifu_info e
-where e.req_date = '20250120'
+where e.req_date = '20250121'
 order by e.create_time desc;
