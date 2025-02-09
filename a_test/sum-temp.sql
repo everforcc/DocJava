857baf109c719e05cdef0711a08d0fe35b4f5548
c.c <718497737@qq.com>
2025-02-09 19:49:52 +0800

    nginx 用户调整

--- a_test/sum-temp.sql
+++ a_test/sum-temp.sql
@@ -1,1 +1,19 @@
 select 1 + 10 sumadd from dual;
+
+-- 0215
+-- 6000-1000（jd）-4000(zyc)
+select 6000-1000-3000 sumadd from dual;
+-- 2000
+-- 0228 打车报销放到zfb，压快两个月的钱
+-- 0308
+select 1000-1000 sumadd from dual;
+
+-- 0315
+select 8000-5000-1000-2000 sumadd from dual;
+-- 1000
+-- 0408
+select 1000-1000 sumadd from dual;
+
+-- 0415
+select 8000-5000 sumadd from dual;
+-- 3000
