1c14a59084f4d7110b8a790e67222fa8360b3ba8
c.c <718497737@qq.com>
2025-02-17 21:46:14 +0800

    后勤管理系统

---
+++ doc_java_project_znkj/24. 店掌柜/仓库管理/ddl/公共字段.sql
@@ -0,0 +1,62 @@
+-- zn_dzg_goods_type
+ALTER TABLE `zn_dzg_goods_type` ADD     create_id     bigint       null comment '创建人id';
+ALTER TABLE `zn_dzg_goods_type` ADD     create_by     varchar(60)  null comment '创建人';
+ALTER TABLE `zn_dzg_goods_type` ADD     create_dept   bigint       null comment '创建部门';
+ALTER TABLE `zn_dzg_goods_type` ADD     create_time   datetime     null comment '创建时间';
+ALTER TABLE `zn_dzg_goods_type` ADD     update_id     bigint       null comment '更新人id';
+ALTER TABLE `zn_dzg_goods_type` ADD     update_by     varchar(60)  null comment '更新人';
+ALTER TABLE `zn_dzg_goods_type` ADD     update_time   datetime     null comment '更新时间';
+ALTER TABLE `zn_dzg_goods_type` ADD     del_flag      tinyint(1)   null comment '删除标记';
+
+-- zn_dzg_goods
+ALTER TABLE `zn_dzg_goods` ADD     create_id     bigint       null comment '创建人id';
+ALTER TABLE `zn_dzg_goods` ADD     create_by     varchar(60)  null comment '创建人';
+ALTER TABLE `zn_dzg_goods` ADD     create_dept   bigint       null comment '创建部门';
+ALTER TABLE `zn_dzg_goods` ADD     create_time   datetime     null comment '创建时间';
+ALTER TABLE `zn_dzg_goods` ADD     update_id     bigint       null comment '更新人id';
+ALTER TABLE `zn_dzg_goods` ADD     update_by     varchar(60)  null comment '更新人';
+ALTER TABLE `zn_dzg_goods` ADD     update_time   datetime     null comment '更新时间';
+ALTER TABLE `zn_dzg_goods` ADD     del_flag      tinyint(1)   null comment '删除标记';
+
+-- zn_dzg_stock
+ALTER TABLE `zn_dzg_stock` ADD     create_id     bigint       null comment '创建人id';
+ALTER TABLE `zn_dzg_stock` ADD     create_by     varchar(60)  null comment '创建人';
+ALTER TABLE `zn_dzg_stock` ADD     create_dept   bigint       null comment '创建部门';
+ALTER TABLE `zn_dzg_stock` ADD     create_time   datetime     null comment '创建时间';
+ALTER TABLE `zn_dzg_stock` ADD     update_id     bigint       null comment '更新人id';
+ALTER TABLE `zn_dzg_stock` ADD     update_by     varchar(60)  null comment '更新人';
+ALTER TABLE `zn_dzg_stock` ADD     update_time   datetime     null comment '更新时间';
+ALTER TABLE `zn_dzg_stock` ADD     del_flag      tinyint(1)   null comment '删除标记';
+
+-- zn_dzg_warehouse_log
+ALTER TABLE `zn_dzg_warehouse_log` ADD     create_id     bigint       null comment '创建人id';
+ALTER TABLE `zn_dzg_warehouse_log` ADD     create_by     varchar(60)  null comment '创建人';
+ALTER TABLE `zn_dzg_warehouse_log` ADD     create_dept   bigint       null comment '创建部门';
+ALTER TABLE `zn_dzg_warehouse_log` ADD     create_time   datetime     null comment '创建时间';
+ALTER TABLE `zn_dzg_warehouse_log` ADD     update_id     bigint       null comment '更新人id';
+ALTER TABLE `zn_dzg_warehouse_log` ADD     update_by     varchar(60)  null comment '更新人';
+ALTER TABLE `zn_dzg_warehouse_log` ADD     update_time   datetime     null comment '更新时间';
+ALTER TABLE `zn_dzg_warehouse_log` ADD     del_flag      tinyint(1)   null comment '删除标记';
+
+-- zn_dzg_check_task
+ALTER TABLE `zn_dzg_check_task` ADD     create_id     bigint       null comment '创建人id';
+ALTER TABLE `zn_dzg_check_task` ADD     create_by     varchar(60)  null comment '创建人';
+ALTER TABLE `zn_dzg_check_task` ADD     create_dept   bigint       null comment '创建部门';
+ALTER TABLE `zn_dzg_check_task` ADD     create_time   datetime     null comment '创建时间';
+ALTER TABLE `zn_dzg_check_task` ADD     update_id     bigint       null comment '更新人id';
+ALTER TABLE `zn_dzg_check_task` ADD     update_by     varchar(60)  null comment '更新人';
+ALTER TABLE `zn_dzg_check_task` ADD     update_time   datetime     null comment '更新时间';
+ALTER TABLE `zn_dzg_check_task` ADD     del_flag      tinyint(1)   null comment '删除标记';
+
+-- zn_dzg_check_task_data
+ALTER TABLE `zn_dzg_check_task_data` ADD     create_id     bigint       null comment '创建人id';
+ALTER TABLE `zn_dzg_check_task_data` ADD     create_by     varchar(60)  null comment '创建人';
+ALTER TABLE `zn_dzg_check_task_data` ADD     create_dept   bigint       null comment '创建部门';
+ALTER TABLE `zn_dzg_check_task_data` ADD     create_time   datetime     null comment '创建时间';
+ALTER TABLE `zn_dzg_check_task_data` ADD     update_id     bigint       null comment '更新人id';
+ALTER TABLE `zn_dzg_check_task_data` ADD     update_by     varchar(60)  null comment '更新人';
+ALTER TABLE `zn_dzg_check_task_data` ADD     update_time   datetime     null comment '更新时间';
+ALTER TABLE `zn_dzg_check_task_data` ADD     del_flag      tinyint(1)   null comment '删除标记';
+
+
+
