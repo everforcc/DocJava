2dfbbe594ab7ab625f691b7e882b72ed7a2a884b
c.c <718497737@qq.com>
2025-02-21 20:54:46 +0800

    书签调整

--- doc_java_project_znkj/21. 园区/13. 支付/退款/132.sql
+++ doc_java_project_znkj/21. 园区/13. 支付/退款/132.sql
@@ -1,5 +1,5 @@
 
 --  ¶©µ¥ 192.168.1.132 ÍË¿î
 select e.hf_seq_id,e.trans_amt,e.create_by,e.req_date,e.* from znkj_huifu_info e
-where e.req_date = '20250209'
+where e.req_date = '20250221'
 order by e.create_time desc;
