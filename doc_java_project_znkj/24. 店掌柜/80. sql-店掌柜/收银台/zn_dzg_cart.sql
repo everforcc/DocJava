911b09d2d3c0e167015e91be3f7f14baebe50d71
c.c <718497737@qq.com>
2025-03-05 20:46:09 +0800

    购物车逻辑调整

---
+++ doc_java_project_znkj/24. 店掌柜/80. sql-店掌柜/收银台/zn_dzg_cart.sql
@@ -0,0 +1,41 @@
+
+-- 商品
+select *
+from zn_dzg_product e
+where e.id = 1894221408124956674
+  and e.barcode = '123456789';
+SELECT *
+FROM zn_dzg_product e
+WHERE e.name = '可口可乐';
+
+-- 价签
+-- 8795687210500
+select *
+from zn_dzg_price_tag e
+where e.code = 1;
+
+-- 233
+-- 1894217210645868545 1894217458202079234 1894217471640629249
+-- 234
+-- 1894217513076158466
+-- 购物车
+select *
+from zn_dzg_cart e;
+
+-- 新增购物车编号
+select e.num
+from zn_dzg_cart e
+where e.num not in (1, 2, 3, 4);
+
+
+-- 购物车商品
+select *
+from zn_dzg_cart_goods e;
+
+-- 新增购物车商品，计算价格
+UPDATE zn_dzg_cart_goods
+SET goods_price = goods_unit_price * (goods_num + 1),
+    goods_num   = goods_num + 1
+WHERE goods_id = 1894221408124956674
+  AND cart_id = 1894217513076158466;
+
