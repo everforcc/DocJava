10ecce0cf4e7d97dc2ccb881a167bd54dfa53f2b
c.c <718497737@qq.com>
2025-03-25 20:55:06 +0800

    Reactor模型

--- doc_java_project_znkj/22. 园区-本地/80. sql-192.168.1.134-本地园区/zn_model.sql
+++ doc_java_project_znkj/22. 园区-本地/80. sql-192.168.1.134-本地园区/zn_model.sql
@@ -20,5 +20,50 @@ select e.model_name,
        e.create_time,
        e.*
 from zn_model e
-where e.model_name = 'CSTB001'
+-- where e.model_name = 'TBSTB001'
 order by e.update_time desc;
+
+-- 1904369224120872962
+select * from zn_model e
+where e.id = 1904369224120872962;
+
+select * from zn_model e
+where e.model_name = 'TBCS';
+
+select * from zn_model e
+where e.parent_id = 1904369224120872962;
+
+select count(*) from zn_model e
+where e.parent_id = 1904369224120872962;
+
+
+select count(*) from zn_model e
+where e.parent_id = 1904374150934900737;
+
+select e.update_time,
+       e.create_time,
+       e.model_name,
+       e.manager_id,
+       e.all_id,
+       e.last_day,
+       e.up_flag,
+       e.up_count,
+       e.* from zn_model e
+order by e.update_time desc;
+
+select e.update_time,
+       e.create_time,
+       e.model_name,
+       e.manager_id,
+       e.all_id,
+       e.last_day,
+       e.up_flag,
+       e.up_count,
+       e.* from zn_model e
+where e.parent_id = 1904374150934900737
+order by e.update_time desc;
+
+
+select count(*) from zn_model e
+where e.parent_id = 1904374150934900737;
+
