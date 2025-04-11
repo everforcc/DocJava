64515ede8a6ec1735930c57799c73c5ed26ce1cc
c.c <718497737@qq.com>
2025-04-11 21:25:20 +0800

    店掌柜退款

--- doc_java_project_znkj/21. 园区/13. 支付/退款/136.sql
+++ doc_java_project_znkj/21. 园区/13. 支付/退款/136.sql
@@ -1,8 +1,10 @@
 
 -- Ô°ÇøÔÆ¶Ë 192.168.1.136 ÍË¿î
-SELECT JSON_UNQUOTE(JSON_EXTRACT(e.other_data,'$.hf_seq_id')) AS hf_seq_id,e.pay_amt,e.req_date,e.create_time,e.other_data,e.*
+SELECT JSON_UNQUOTE(JSON_EXTRACT(e.other_data,'$.hf_seq_id')) AS hf_seq_id,e.pay_amt,e.huifuId,e.req_date,e.create_time,e.other_data,e.*
 FROM zn_pay_order_record e
-WHERE e.req_date = '20250221' and e.pay_amt is not null
+WHERE
+   --   e.req_date = '20250221' and
+      e.pay_amt is not null
 ORDER BY e.create_time DESC;
 
 --
@@ -13,3 +15,7 @@ ORDER BY e.create_time DESC;
 --
 select e.create_time,e.* from zn_pay_order e
 order by e.create_time desc;
+
+SELECT json_unquote(JSON_EXTRACT(e.other_data,'$.hf_seq_id')) as other_data,e.req_date,e.huifuId FROM zn_pay_order_record e
+WHERE    e.pay_amt is not null
+ORDER BY e.create_time DESC;
--- doc_java_project_znkj/21. 园区/80. sql-192.168.1.136-园区/zn_pay_order_record.sql
+++ doc_java_project_znkj/21. 园区/80. sql-192.168.1.136-园区/zn_pay_order_record.sql
@@ -9,10 +9,15 @@ where e.order_type = 3;
 
 select e.status,e.create_time,e.* from zn_pay_order e
 where e.order_type = 5
-order by e.create_time desc
-;
+order by e.create_time desc;
+-- 1106683457842585600
+select * from zn_pay_order_record e
+where e.req_seqId = 1106683457842585600;
+
+
+
 -- 1102743280749256704
-select e.status,e.pay_amt,
+select e.status,e.pay_amt,e.req_seqId,
        JSON_EXTRACT(e.other_data, '$.req_seq_id') AS a,
        JSON_EXTRACT(e.other_data, '$.hf_seq_id') AS b ,
        JSON_EXTRACT(e.other_data, '$.resp_desc') AS c,
