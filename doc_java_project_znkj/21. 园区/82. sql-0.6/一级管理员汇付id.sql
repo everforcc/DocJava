69a6d6c0ad66cc212bebd7e90c8614d602264b42
c.c <718497737@qq.com>
2025-04-23 21:49:36 +0800

    店掌柜支付测试

--- doc_java_project_znkj/21. 园区/82. sql-0.6/一级管理员汇付id.sql
+++ doc_java_project_znkj/21. 园区/82. sql-0.6/一级管理员汇付id.sql
@@ -1,12 +1,12 @@
 
 -- 增加 指定sn 支付用户
 
--- 1. manager_phone
+-- 1. 根据sn查出来manager_phone
 select e.manager_phone,e.* from zn_device_sn e where e.zn_device_sn_var = 'PRE758294763';
 
--- 2.
+-- 2. 根据手机号查出来 user_id
 select e.user_id,e.* from sys_user e where e.user_name like '%13103785939';
 
--- 3. 复制 huifu_id 是 6666000151824676 的，修改id 为 sys_user
+-- 3. 复制 huifu_id 是 6666000151824676 的，修改id 为 sys_user.user_id
 select * from zn_pay_user e where e.id = 1077596090997547008 ;
 
