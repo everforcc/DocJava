4f1e104e85a451458adbddf4c38a109e2e1c094e
c.c <718497737@qq.com>
2025-07-14 19:59:24 +0800

    test 文件

--- a_test/sum-temp.sql
+++ a_test/sum-temp.sql
@@ -1,19 +1,2 @@
+ï»¿
 select 1 + 10 sumadd from dual;
-
--- 0215
--- 6000-1000£¨jd£©-4000(zyc)
-select 6000-1000-3000 sumadd from dual;
--- 2000
--- 0228 ´ò³µ±¨Ïú·Åµ½zfb£¬Ñ¹¿ìÁ½¸öÔÂµÄÇ®
--- 0308
-select 1000-1000 sumadd from dual;
-
--- 0315
-select 8000-5000-1000-2000 sumadd from dual;
--- 1000
--- 0408
-select 1000-1000 sumadd from dual;
-
--- 0415
-select 8000-5000 sumadd from dual;
--- 3000
