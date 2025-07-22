d8d24ea0e939128faa12597a352199ae2371df85
c.c <718497737@qq.com>
2025-07-22 20:55:38 +0800

    znkj

---
+++ doc_java_project_znkj/21. 园区/19. 收费改版/ddl/zn_fee_type.sql
@@ -0,0 +1,7 @@
+INSERT INTO `zn_fee_type` (`code`, `name`, `remark`, `del_flag`) VALUES ('5-1', '超市', NULL, 0);
+INSERT INTO `zn_fee_type` (`code`, `name`, `remark`, `del_flag`) VALUES ('5-2', '餐饮', NULL, 0);
+INSERT INTO `zn_fee_type` (`code`, `name`, `remark`, `del_flag`) VALUES ('5-3', '收钱', NULL, 0);
+INSERT INTO `zn_fee_type` (`code`, `name`, `remark`, `del_flag`) VALUES ('7-1', '积分充值', NULL, 0);
+INSERT INTO `zn_fee_type` (`code`, `name`, `remark`, `del_flag`) VALUES ('7-2', '知识库续费', NULL, 0);
+INSERT INTO `zn_fee_type` (`code`, `name`, `remark`, `del_flag`) VALUES ('8-1', '预存金豆', NULL, 0);
+INSERT INTO `zn_fee_type` (`code`, `name`, `remark`, `del_flag`) VALUES ('9-1', '大模型充值', NULL, 0);
