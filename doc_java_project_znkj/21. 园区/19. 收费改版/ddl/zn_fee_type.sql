7aeb54dfc2c05d9a2ca437a0f20cf63851b0069e
c.c <718497737@qq.com>
2025-07-29 17:12:19 +0800

    rk

--- doc_java_project_znkj/21. 园区/19. 收费改版/ddl/zn_fee_type.sql
+++ doc_java_project_znkj/21. 园区/19. 收费改版/ddl/zn_fee_type.sql
@@ -5,4 +5,12 @@ INSERT INTO `zn_fee_type` (`code`, `name`, `remark`, `del_flag`) VALUES ('7-1', 
 INSERT INTO `zn_fee_type` (`code`, `name`, `remark`, `del_flag`) VALUES ('7-2', '知识库续费', NULL, 0);
 INSERT INTO `zn_fee_type` (`code`, `name`, `remark`, `del_flag`) VALUES ('8-1', '预存金豆', NULL, 0);
 INSERT INTO `zn_fee_type` (`code`, `name`, `remark`, `del_flag`) VALUES ('9-1', '大模型充值', NULL, 0);
-INSERT INTO `zn_fee_type` (`code`, `name`, `remark`, `del_flag`) VALUES ('10-1', '云通道开通/赠送', NULL, 0);
+INSERT INTO `zn_fee_type` (code, name, remark, del_flag) VALUES ('10-1', '门禁机', null, 0);
+INSERT INTO `zn_fee_type` (code, name, remark, del_flag) VALUES ('10-2', '车牌相机', null, 0);
+INSERT INTO `zn_fee_type` (code, name, remark, del_flag) VALUES ('10-3', '梯控', null, 0);
+INSERT INTO `zn_fee_type` (code, name, remark, del_flag) VALUES ('10-4', 'IOT设备', null, 0);
+INSERT INTO `zn_fee_type` (code, name, remark, del_flag) VALUES ('10-5', '能耗设备', null, 0);
+INSERT INTO `zn_fee_type` (code, name, remark, del_flag) VALUES ('10-7', '考勤管理', null, 0);
+INSERT INTO `zn_fee_type` (code, name, remark, del_flag) VALUES ('10-9', '音响设备', null, 0);
+INSERT INTO `zn_fee_type` (code, name, remark, del_flag) VALUES ('10-11', '摄像机直播', null, 0);
+INSERT INTO `zn_fee_type` (code, name, remark, del_flag) VALUES ('10-12', 'AI事件', null, 0);
