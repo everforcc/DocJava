23094addddff9bbe09fda158ba92db0a57aca863
c.c <718497737@qq.com>
2025-09-11 21:33:55 +0800

    rk 关系代数

--- doc_java_project_znkj/22. 园区-本地/80. sql-192.168.0.30-本地园区/权限.sql
+++ doc_java_project_znkj/22. 园区-本地/80. sql-192.168.0.30-本地园区/权限.sql
@@ -1,3 +1,7 @@
+
+-- 查看正在连接的客户端
+SHOW PROCESSLIST;
+
 -- 1. 查看当前root用户情况
 SELECT User, Host FROM mysql.user WHERE User = 'root';
 SELECT * FROM mysql.user WHERE User = 'root';
@@ -10,15 +14,16 @@ CREATE USER 'root'@'localhost' IDENTIFIED BY 'znkj123456';
 
 -- 4. 创建允许从指定IP访问的root用户
 CREATE USER 'root'@'192.168.1.188' IDENTIFIED BY 'znkj123456';
-
 CREATE USER 'root'@'192.168.1.60' IDENTIFIED BY 'znkj123456';
-
 CREATE USER 'root'@'%' IDENTIFIED BY 'znkj123456';
 
 -- 5. 授予相应权限
+-- 允许本机访问
 GRANT ALL PRIVILEGES ON *.* TO 'root'@'localhost' WITH GRANT OPTION;
+-- 允许指定IP访问
 GRANT ALL PRIVILEGES ON *.* TO 'root'@'192.168.1.188' WITH GRANT OPTION;
 GRANT ALL PRIVILEGES ON *.* TO 'root'@'192.168.1.60' WITH GRANT OPTION;
+-- 允许所有IP访问
 GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' WITH GRANT OPTION;
 
 -- 6. 刷新权限
