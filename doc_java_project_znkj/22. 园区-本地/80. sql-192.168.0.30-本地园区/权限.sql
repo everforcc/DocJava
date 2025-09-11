85f96610b48891322ab2bdceeed4796cf4c1156b
c.c <718497737@qq.com>
2025-09-11 20:03:35 +0800

    znkj lkl

---
+++ doc_java_project_znkj/22. 园区-本地/80. sql-192.168.0.30-本地园区/权限.sql
@@ -0,0 +1,25 @@
+-- 1. 查看当前root用户情况
+SELECT User, Host FROM mysql.user WHERE User = 'root';
+SELECT * FROM mysql.user WHERE User = 'root';
+
+-- 2. 删除不限制来源的root用户（如果存在）
+DELETE FROM mysql.user WHERE User = 'root' AND Host = '%';
+
+-- 3. 创建允许从本机访问的root用户
+CREATE USER 'root'@'localhost' IDENTIFIED BY 'znkj123456';
+
+-- 4. 创建允许从指定IP访问的root用户
+CREATE USER 'root'@'192.168.1.188' IDENTIFIED BY 'znkj123456';
+
+CREATE USER 'root'@'192.168.1.60' IDENTIFIED BY 'znkj123456';
+
+CREATE USER 'root'@'%' IDENTIFIED BY 'znkj123456';
+
+-- 5. 授予相应权限
+GRANT ALL PRIVILEGES ON *.* TO 'root'@'localhost' WITH GRANT OPTION;
+GRANT ALL PRIVILEGES ON *.* TO 'root'@'192.168.1.188' WITH GRANT OPTION;
+GRANT ALL PRIVILEGES ON *.* TO 'root'@'192.168.1.60' WITH GRANT OPTION;
+GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' WITH GRANT OPTION;
+
+-- 6. 刷新权限
+FLUSH PRIVILEGES;
