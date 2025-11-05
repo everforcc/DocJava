24a238dcca468e85df40451e0294b9610b772df5
c.c <718497737@qq.com>
2025-11-05 17:49:05 +0800

    lkl 文档优化

--- doc_java_project_znkj/32. 拉卡拉/ddl/zn_pay_order_record.sql
+++ doc_java_project_znkj/32. 拉卡拉/ddl/zn_pay_order_record.sql
@@ -6,3 +6,13 @@ ALTER TABLE `zn_pay_order_record`
     ADD lkl_merchant_no varchar(255) null comment '拉卡拉商户号';
 ALTER TABLE `zn_pay_order_record`
     ADD lkl_term_no varchar(255) null comment '拉卡拉终端号';
+ALTER TABLE `zn_pay_order_record`
+    ADD lkl_split_req json null comment '拉卡拉分账请求';
+ALTER TABLE `zn_pay_order_record`
+    ADD lkl_split_res json null comment '拉卡拉分账响应';
+ALTER TABLE `zn_pay_order_record`
+    ADD lkl_split_callback json null comment '拉卡拉分账回调';
+ALTER TABLE `zn_pay_order_record`
+    ADD lkl_split_status varchar(255) null comment '拉卡拉分账状态';
+ALTER TABLE `zn_pay_order_record`
+    ADD lkl_log_no varchar(255) null comment '拉卡拉分账流水号';
