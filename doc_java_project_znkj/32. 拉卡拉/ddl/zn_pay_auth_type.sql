be98dc97f15d62b492c4721114089b5394ff7069
c.c <718497737@qq.com>
2025-11-06 14:44:07 +0800

    lkl 文档优化

--- doc_java_project_znkj/32. 拉卡拉/ddl/zn_pay_auth_type.sql
+++ doc_java_project_znkj/32. 拉卡拉/ddl/zn_pay_auth_type.sql
@@ -1,19 +1,19 @@
 -- auto-generated definition
 create table zn_pay_auth_type
 (
-    id                         bigint auto_increment comment '唯一标识'
+    id          bigint auto_increment comment '唯一标识'
         primary key,
-    user_id                    bigint                                 null comment '用户id 每个用户一行数据参数',
-    type                       tinyint                                null comment '0 用户自选 1 汇付 2 拉卡拉',
-    create_id                  bigint                                 null comment '创建者ID',
-    create_by                  varchar(64) charset utf8mb4 default '' null comment '创建者',
-    create_time                datetime                               null comment '创建时间',
-    create_dept                bigint                                 null comment '创建部门',
-    update_id                  bigint                                 null comment '更新者ID',
-    update_by                  varchar(64) charset utf8mb4 default '' null comment '更新者',
-    update_time                datetime                               null comment '更新时间',
-    del_flag                   tinyint(1)                  default 0  null comment '删除标志（0代表存在 1代表删除）',
-    remark                     text                                   null comment '备注'
+    user_id     bigint                                 null comment '用户id 每个用户一行数据参数',
+    type        tinyint                                null comment '0 用户自选 1 汇付 2 拉卡拉',
+    create_id   bigint                                 null comment '创建者ID',
+    create_by   varchar(64) charset utf8mb4 default '' null comment '创建者',
+    create_time datetime                               null comment '创建时间',
+    create_dept bigint                                 null comment '创建部门',
+    update_id   bigint                                 null comment '更新者ID',
+    update_by   varchar(64) charset utf8mb4 default '' null comment '更新者',
+    update_time datetime                               null comment '更新时间',
+    del_flag    tinyint(1)                  default 0  null comment '删除标志（0代表存在 1代表删除）',
+    remark      text                                   null comment '备注'
 )
     comment '支付开户类型 汇付拉卡拉' collate = utf8mb4_unicode_ci;
 
--- doc_java_project_znkj/32. 拉卡拉/ddl/zn_pay_user_lkl.sql
+++ doc_java_project_znkj/32. 拉卡拉/ddl/zn_pay_user_lkl.sql
@@ -1,24 +1,23 @@
--- zn_pay_user_lkl
 -- auto-generated definition
 create table zn_pay_user_lkl
 (
-    id           bigint                                 not null comment '唯一标识'
+    id          bigint                                 not null comment '唯一标识'
         primary key,
-    user_id      bigint                                 not null comment '用户id',
-    merchant_no     varchar(255)                           null comment '拉卡拉商户号',
-    term_no    varchar(255)                                     null comment '拉卡拉设备号',
-    receiver_no varchar(255)                                     null comment '拉卡拉分账号',
-    role_id      bigint                                 null comment '经销商用户身份，1：市场经理，2：城市合伙人，3：地区代理商（经销商）',
-    parent_Id    bigint                                 null comment '上级id（市场经理是为空)',
-    remark       varchar(255)                           null comment '备注',
-    create_id    bigint                                 null comment '创建者ID',
-    create_by    varchar(64) charset utf8mb4 default '' null comment '创建者',
-    create_time  datetime                               null comment '创建时间',
-    create_dept  bigint                                 null comment '创建部门',
-    update_id    bigint                                 null comment '更新者ID',
-    update_by    varchar(64) charset utf8mb4 default '' null comment '更新者',
-    update_time  datetime                               null comment '更新时间',
-    del_flag     tinyint(1)                  default 0  null
+    user_id     bigint                                 not null comment '用户id',
+    merchant_no varchar(255)                           null comment '拉卡拉商户号',
+    term_no     varchar(255)                           null comment '拉卡拉设备号',
+    receiver_no varchar(255)                           null comment '拉卡拉分账号',
+    role_id     bigint                                 null comment '经销商用户身份，1：市场经理，2：城市合伙人，3：地区代理商（经销商）',
+    parent_Id   bigint                                 null comment '上级id（市场经理是为空)',
+    remark      varchar(255)                           null comment '备注',
+    create_id   bigint                                 null comment '创建者ID',
+    create_by   varchar(64) charset utf8mb4 default '' null comment '创建者',
+    create_time datetime                               null comment '创建时间',
+    create_dept bigint                                 null comment '创建部门',
+    update_id   bigint                                 null comment '更新者ID',
+    update_by   varchar(64) charset utf8mb4 default '' null comment '更新者',
+    update_time datetime                               null comment '更新时间',
+    del_flag    tinyint(1)                  default 0  null
 )
     comment '订单用户拉卡拉' collate = utf8mb4_unicode_ci;
 
